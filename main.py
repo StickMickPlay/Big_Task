e5bbe0a00d58a1f3286d0f249fda35f0c6099949
student <ilya.stukovin@mail.ru>
2025-02-22 15:04:20 +0300

    create mainwindow

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 import requests
 from urllib.parse import urlencode
-import pygame
+
 
 
 def main():
@@ -31,21 +31,8 @@ def show_map(ll_spn=None) -> None:
 
     # Запишем полученное изображение в файл.
     map_file = "map.png"
-    with open(map_file, "wb") as file:
-        file.write(response.content)
 
-    # Инициализируем pygame
-    pygame.init()
-    screen = pygame.display.set_mode((600, 450))
-    # Рисуем картинку, загружаемую из только что созданного файла.
-    screen.blit(pygame.image.load(map_file), (0, 0))
-    # Переключаем экран и ждем закрытия окна.
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    pygame.quit()
 
-    # Удаляем за собой файл с изображением.
     os.remove(map_file)
 
 
